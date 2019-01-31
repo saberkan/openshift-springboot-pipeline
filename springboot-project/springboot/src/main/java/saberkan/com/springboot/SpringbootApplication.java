b9031d46467391c56265b928f0d26f5b10fd2d58
Salahddine ABERKAN <salahddine.aberkan@gmail.com>
2019-02-01 00:20:31 +0100

    Adding springboot project

---
+++ springboot-project/springboot/src/main/java/saberkan/com/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package saberkan.com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
+
